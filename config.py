d5170b9888cd052d2d01e34eb7f46cdc8cc397d6
Kosuke Fujita <thisis.skfk03@gmail.com>
2016-10-10 00:57:24 +0900

    なんとかTrue

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 #Running on http://127.0.0.1:5000/
