d1304a55ebabd8ae6742fbccb5ccb752c13d4422
Kosuke Fujita <thisis.skfk03@gmail.com>
2016-10-20 02:05:38 +0900

    warning

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'hogehoge'
-#SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/todo'
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/todo'
+#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = True
